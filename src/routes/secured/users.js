b10a078e4df98f458f3cb0172d76f958cbc34a14
Aymene <aymene01@hotmail.fr>
2021-03-25 11:11:16 +0100

    get user route

--- src/routes/secured/users.js
+++ src/routes/secured/users.js
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const api = Router()
 
-api.get('/', (req, res) => {
-    res.send('hello world')
+api.get('/:id', async (req, res) => {
+    try {
+        const prisma = new PrismaClient()
+        const id = parseInt(req.params.id, 10)
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+        if(!user) return res.status(404).send(`the user with the id ${id} was not found`)
+        res.json({user: user})
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 export default api
